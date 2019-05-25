fb8d99f792e4d489e6a44bae162640ddf79d0d85
Simon Wendel <mail@simonwendel.se>
2019-05-25 15:22:23 +0200

    feat: add ISerialPort interface on SoftwareSerial

---
+++ src/hardware/ISerialPort.h
@@ -0,0 +1,37 @@
+/*
+ * Sun to USB keyboard adapter
+ * Copyright (C) 2019  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+namespace hardware
+{
+    class ISerialPort
+    {
+    public:
+        virtual int read() = 0;
+        virtual size_t write(uint8_t byte) = 0;
+        virtual size_t write(const uint8_t *buffer, size_t size) = 0;
+
+        virtual ~ISerialPort()
+        {
+        }
+    };
+} // namespace hardware
---
+++ src/hardware/SerialPort.h
@@ -0,0 +1,43 @@
+/*
+ * Sun to USB keyboard adapter
+ * Copyright (C) 2019  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "ISerialPort.h"
+
+#include <SoftwareSerial.h>
+#include <memory>
+#include <stdint.h>
+
+namespace hardware
+{
+    class SerialPort : public ISerialPort
+    {
+    private:
+        std::unique_ptr<SoftwareSerial> serial;
+
+    public:
+        SerialPort::SerialPort(uint8_t receivePin,
+                               uint8_t transmitPin,
+                               bool inverse_logic = false);
+
+        int read() override;
+        size_t write(uint8_t byte) override;
+        size_t write(const uint8_t *buffer, size_t size) override;
+    };
+} // namespace hardware
