fa0d3e05f4fbd819838cc772701cb58288c072c4
Simon Wendel <mail@simonwendel.se>
2019-05-27 19:29:30 +0200

    fix: make cpp files in nested dirs

--- Project.mk
+++ Project.mk
@@ -25,31 +25,52 @@ BOARD_TAG				= itsybitsy32u4_5V
 MONITOR_PORT			= /dev/ttyACM0
 
 # this should be the dir of this Project.mk file
-SELF_DIR				:= $(dir $(lastword $(MAKEFILE_LIST)))
-BUILD_DIR				= $(SELF_DIR)build
+ROOT_DIR				:= $(dir $(lastword $(MAKEFILE_LIST)))
+BUILD_DIR				= $(ROOT_DIR)build
 
 CURRENT_DIR				= $(shell basename $(CURDIR))
-OBJDIR					= $(BUILD_DIR)/$(CURRENT_DIR)
-
-CFLAGS_STD				= -std=gnu11
-CXXFLAGS_STD			= -std=gnu++17
-CXXFLAGS				+= -I $(SELF_DIR)/lib
-CXXFLAGS				+= -pedantic -Wall -Wextra
-LDFLAGS					+= -fdiagnostics-color
+OBJDIR					= $(BUILD_DIR)
 
 # check out the Arduino-Makefile RPM from my config github repo at
 # https://github.com/simonwendel/configs/tree/master/Packages
 include /usr/share/arduino/Arduino.mk
 
+CFLAGS_STD				= -std=gnu11
+CXXFLAGS_STD			= -std=gnu++17
+CPPFLAGS				+= -I $(ROOT_DIR)lib
+CPPFLAGS				+= -pedantic -Wall -Wextra
+LDFLAGS					+= -fdiagnostics-color
+
+####
+# What follows are a bunch of hacks to make stupid shit work.
+####
+
 # hack to make sure HID-Project.h doesn't throw. should probably match
 # the version of your actual Arduino SDK install
-CXXFLAGS				+= -DARDUINO=10805 # v1.8.5
+CPPFLAGS				+= -DARDUINO=10805 # v1.8.5
 
 # hack for us to be able to stub out Arduino.h if needed for testing
 # here we unstub it
-CXXFLAGS				+= -USTUB_ARDUINO
+CPPFLAGS				+= -USTUB_ARDUINO
 
+# hacks to compile c++ files in some tree structure, instead of 
+# all files directly in src/
+define find_cpp_files
+	$(shell find $(1) -maxdepth 1 -type f -name '*.cpp')
+endef
+
+SOURCE_FOLDERS = lib/ src/hardware src/adapter
+
+SRCS			= $(foreach dir, $(SOURCE_FOLDERS), $(call find_cpp_files, $(ROOT_DIR)$(dir)))
+LOCAL_CPP_SRCS  += $(subst $(ROOT_DIR),,$(SRCS))
+
+$(OBJDIR)/$(TARGET).elf: $(LOCAL_OBJ_FILES)
+
+$(LOCAL_OBJ_FILES): $(LOCAL_CPP_SRCS)
+
+$(LOCAL_CPP_SRCS):
+	@$(MKDIR) -p $(OBJDIR)/$(dir $@)
+	$(CXX) -MMD -c $(CPPFLAGS) $(ROOT_DIR)/$@ -o $(OBJDIR)/$@.o
 
 clean::
-	$(info Removing $(BUILD_DIR) if empty...)
-	$(shell rmdir $(BUILD_DIR) &>/dev/null)
+	$(shell rm -rf $(BUILD_DIR) &>/dev/null)
