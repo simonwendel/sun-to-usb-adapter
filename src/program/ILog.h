11427a89fda5599dd23d3d26e9e1d5dcec38dd88
Simon Wendel <mail@simonwendel.se>
2019-05-30 21:15:49 +0200

    feat: add ILog interface class

---
+++ src/program/ILog.h
@@ -0,0 +1,30 @@
+/*
+ * Sun to USB keyboard adapter
+ * Copyright (C) 2019  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <arduino-platform.h>
+
+namespace program
+{
+    class ILog
+    {
+    public:
+        virtual void error(String message) = 0;
+    };
+} // namespace program
