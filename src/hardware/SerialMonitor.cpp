240be88bd8801f8f5ab1e3219edf392999c201e9
Simon Wendel <mail@simonwendel.se>
2019-05-31 20:39:52 +0200

    feat: add serial debug monitor as ISerialPort

---
+++ src/hardware/SerialMonitor.cpp
@@ -0,0 +1,40 @@
+/*
+ * Sun to USB keyboard adapter
+ * Copyright (C) 2019  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "SerialMonitor.h"
+
+#include <arduino-platform.h>
+#include <stdint.h>
+
+namespace hardware
+{
+    int SerialMonitor::read()
+    {
+        return Serial.read();
+    }
+
+    size_t SerialMonitor::write(uint8_t byte)
+    {
+        return Serial.write(byte);
+    }
+
+    size_t SerialMonitor::write(const uint8_t *buffer, size_t size)
+    {
+        return Serial.write(buffer, size);
+    }
+} // namespace hardware
