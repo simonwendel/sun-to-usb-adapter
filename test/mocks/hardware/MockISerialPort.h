dc9b449b291525b8f0e558a1ccfb4777ae546c01
Simon Wendel <mail@simonwendel.se>
2019-06-01 21:55:13 +0200

    feat: add Log class to output text via serial port

---
+++ test/mocks/hardware/MockISerialPort.h
@@ -0,0 +1,39 @@
+/*
+ * Sun to USB keyboard adapter
+ * Copyright (C) 2019  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "../../../src/hardware/ISerialPort.h"
+
+#include <arduino-platform.h>
+#include <gmock/gmock.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+namespace hardware_mocks
+{
+    class MockISerialPort : public hardware::ISerialPort
+    {
+    public:
+        MOCK_METHOD0(read, int());
+        MOCK_METHOD1(write, size_t(uint8_t));
+        MOCK_METHOD2(write, size_t(const uint8_t *, size_t));
+        MOCK_METHOD1(print, size_t(const String &));
+        MOCK_METHOD1(println, size_t(const String &));
+    };
+} // namespace hardware_mocks
---
+++ test/program/LogTests.cpp
@@ -0,0 +1,51 @@
+/*
+ * Sun to USB keyboard adapter
+ * Copyright (C) 2019  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../src/program/Log.h"
+#include "../mocks/hardware/MockISerialPort.h"
+
+#include <arduino-platform.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <stdint.h>
+
+namespace program_tests
+{
+    using namespace testing;
+
+    class program_Log : public ::testing::Test
+    {
+    public:
+        hardware_mocks::MockISerialPort serialPort;
+        program::Log sut{&serialPort};
+    };
+
+    TEST_F(program_Log, error_GivenSerialPort_WritesToSerialPort)
+    {
+        String prefix{"ERROR: "};
+        String message{"This is an error!"};
+        {
+            InSequence seq;
+
+            EXPECT_CALL(serialPort, print(prefix));
+            EXPECT_CALL(serialPort, println(message));
+        }
+
+        sut.error(message);
+    }
+} // namespace program_tests
