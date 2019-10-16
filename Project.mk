70bf887abe9226348e95ec3554c497e4ecf80b80
Simon Wendel <mail@simonwendel.se>
2019-10-16 21:28:56 +0200

    feat: install libs from Make file and tidy up

--- Project.mk
+++ Project.mk
@@ -14,39 +14,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+SOURCE_FOLDERS 			= lib src/hardware src/adapter src/hardware/timers
+3RD_PARTY_LIBS			= HID-Project
 ARDUINO_LIBS			= SoftwareSerial \
-						  HID \
-						  HID-Project
+						  HID
 
+# defining hardware
 ALTERNATE_CORE			= adafruit
 ALTERNATE_CORE_PATH		= $(ARDUINO_DIR)/hardware/adafruit/avr
-
 BOARD_TAG				= itsybitsy32u4_5V
 MONITOR_PORT			= /dev/ttyACM0
 
-# this should be the dir of this Project.mk file
+# locate directories relative Project.mk
 ROOT_DIR				:= $(dir $(lastword $(MAKEFILE_LIST)))
-
 CURRENT_DIR				= $(shell basename $(CURDIR))
-
 BUILD_DIR				= $(ROOT_DIR)build
 OBJDIR					= $(BUILD_DIR)
 
-# check out the Arduino-Makefile RPM from my config github repo at
-# https://github.com/simonwendel/configs/tree/master/Packages
-include /usr/share/arduino/Arduino.mk
-
 CFLAGS_STD				= -std=gnu11
 CPPFLAGS_STD			= -std=gnu++1z
-
 CPPFLAGS				+= -I $(ROOT_DIR)lib/stubs
 CPPFLAGS				+= -pedantic -Wall -Wextra
 CPPFLAGS				+= $(CPPFLAGS_STD)
 LDFLAGS					+= -fdiagnostics-color
-
-####
-# What follows are a bunch of hacks to make stupid shit work.
-####
 
 # hack to make sure HID-Project.h doesn't throw. should probably match
 # the version of your actual Arduino SDK install
@@ -56,24 +46,33 @@ CPPFLAGS				+= -DARDUINO=10805 # v1.8.5
 # here we unstub it
 CPPFLAGS				+= -USTUB_ARDUINO
 
-# hacks to compile c++ files in some tree structure, instead of 
+# these need to be evaluated before including Arduino.mk or stuff will break
+_ := $(shell arduino-cli lib update-index)
+_ := $(foreach lib, $(3RD_PARTY_LIBS), $(shell arduino-cli lib install $(lib)))
+
+# check out the Arduino-Makefile RPM from my config github repo at
+# https://github.com/simonwendel/configs/tree/master/Packages
+ARDUINO_LIBS			+= $(3RD_PARTY_LIBS)
+USER_LIB_PATH			+= $(wildcard ~)/Arduino/libraries
+include /usr/share/arduino/Arduino.mk
+
+# hacks to compile C++ files in some tree structure, instead of 
 # all files directly in src/
 define find_cpp_files
 	$(shell find $(1) -maxdepth 1 -type f -name '*.cpp')
 endef
 
-SOURCE_FOLDERS = lib/ src/hardware src/adapter src/hardware/timers
+SRCS					= $(foreach dir, $(SOURCE_FOLDERS), $(call find_cpp_files, $(ROOT_DIR)$(dir)))
+LOCAL_CPP_SRCS  		+= $(subst $(ROOT_DIR),,$(SRCS))
 
-SRCS			= $(foreach dir, $(SOURCE_FOLDERS), $(call find_cpp_files, $(ROOT_DIR)$(dir)))
-LOCAL_CPP_SRCS  += $(subst $(ROOT_DIR),,$(SRCS))
-
+# and make rules!
 $(OBJDIR)/$(TARGET).elf: $(LOCAL_OBJ_FILES)
 
 $(LOCAL_OBJ_FILES): $(LOCAL_CPP_SRCS)
 
 $(LOCAL_CPP_SRCS):
 	@$(MKDIR) -p $(OBJDIR)/$(dir $@)
-	$(CXX) -MMD -c $(CPPFLAGS) $(ROOT_DIR)/$@ -o $(OBJDIR)/$@.o
+	$(CXX) -MMD -c $(CPPFLAGS) $(ROOT_DIR)$@ -o $(OBJDIR)/$@.o
 
 clean::
 	$(shell rm -rf $(BUILD_DIR) &>/dev/null)
